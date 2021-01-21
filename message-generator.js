c058d1a510c5e70d5584f5071087ff07ecbfaa9a
Elisabeth Rudd <elisabethrudd26@gmail.com>
2021-01-21 12:24:51 -0500

    "Link files and add components"

--- message-generator.js
+++ message-generator.js
@@ -1,1 +1,18 @@
-console.log("Mr Waffles is a cute cat ");
+const nouns = [
+  "iguana",
+  "platypus",
+  " piece of bacon",
+  "seagull",
+  "dumpster",
+  "hamster",
+];
+const adjectives = [
+  "fool",
+  "genius",
+  "attractive",
+  "awkward",
+  "interesting",
+  "bodacious",
+];
+const fillerNoun = nouns[Math.floor(Math.random() * 6)];
+console.log(fillerNoun);
